8058f1f9e248683c9a641dce78a38dd135d30562
АзимБулатов <azimbulatov21@gmail.com>
2024-05-12 16:40:31 +0300

    создание экрана криложения

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello World!")
+import pygame
+import sys
+
+# Инициализация Pygame
+pygame.init()
+
+# Размеры окна
+width = 400
+height = 600
+
+# Цвета
+white = (255, 255, 255)
+
+# Создание окна
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Тестовый экран мобильного приложения")
+
+# Основной цикл программы
+while True:
+    # Обработка событий
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Заливка экрана белым цветом
+    screen.fill(white)
+
+    # Обновление экрана
+    pygame.display.flip()
